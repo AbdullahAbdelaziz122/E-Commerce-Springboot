9759491265435cc98fc4f363d4f27aaf1728e107
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-18 12:43:10 +0200

    Added Global Exception Handler and handeled BadCredential

---
+++ src/main/java/com/abdullah/ShoppingCart/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.abdullah.ShoppingCart.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.abdullah.ShoppingCart.domain.dtos.ApiErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiErrorResponse> handleBadCreditialException(BadCredentialsException ex) {
+
+		ApiErrorResponse error = ApiErrorResponse.builder().status(HttpStatus.UNAUTHORIZED.value())
+				.message("Incorrect Username or Password").timestamp(LocalDateTime.now()).build();
+
+		return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiErrorResponse> handleGenericException(Exception ex) {
+	    ApiErrorResponse error = ApiErrorResponse.builder()
+	            .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+	            .message("An internal error occurred") 
+	            .timestamp(LocalDateTime.now())
+	            .build();
+
+	    return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
