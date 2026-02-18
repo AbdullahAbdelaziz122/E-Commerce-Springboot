1e165eaec6d74abc23543787341c4b43fdb96fba
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-18 12:46:48 +0200

    Add IAuthenticationService

---
+++ src/main/java/com/abdullah/ShoppingCart/services/IAuthenticationService.java
@@ -0,0 +1,8 @@
+package com.abdullah.ShoppingCart.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface IAuthenticationService {
+	UserDetails authenticate(String email, String password);
+	String generateToken(UserDetails userDetails);
+}
