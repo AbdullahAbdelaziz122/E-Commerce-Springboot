36f3ca71b8b6d879a9aaf80862a538e2cbbc98c6
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-18 05:14:18 +0200

    initial commit

---
+++ src/main/java/com/abdullah/ShoppingCart/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.abdullah.ShoppingCart.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abdullah.ShoppingCart.domain.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+	
+}
